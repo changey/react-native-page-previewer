6bdaf722ae2b27336b80463d0c475fcced8fa102
Eric Chang <eric@gofoward.com>
2016-07-22 17:10:34 -0700

    added cheerio

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 
 import { NativeModules } from 'react-native';
+var cheerio = require('cheerio-without-node-native');
 
 // const { RNReactNativePagePreviewer } = NativeModules;
 
 var RNReactNativePagePreviewer = {
   getTitle: function() {
+    fetch("https://news.ycombinator.com", {method: "GET"})
+      .then((response) => {
+          console.log("foo response", "POST Response",
+          "Response Body -> ")
+
+          var html = response._bodyInit;
+          var doc = cheerio.load(html);
+          var title = getTitle(doc);
+          console.log("foo title", title)
+    })
     console.log("foo title");
   },
 };
